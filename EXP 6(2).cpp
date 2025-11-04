23358aa3b07a4e5124220d2c255f719ef7538dd0
faizan007-123 <mhatefaizan@gmail.com>
2025-11-04 20:48:57 +0530

    Create EXP 6(2).cpp

---
+++ EXP 6(2).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+public:
+    Vehicle() {
+        cout << "This is a Vehicle\n";
+    }
+};
+
+// First subclass
+class Car : public Vehicle {
+public:
+    Car() {
+        cout << "This Vehicle is Car\n";
+    }
+};
+
+// Second subclass
+class Bus : public Vehicle {
+public:
+    Bus() {
+        cout << "This Vehicle is Bus\n";
+    }
+};
+
+int main() {
+    // Creating objects of subclasses will also invoke base class constructor
+    Car obj1;
+    Bus obj2;
+    return 0;
+}
+
