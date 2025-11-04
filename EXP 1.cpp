4636573e64c561e7a9b892ffae0356554fafb8fa
faizan007-123 <mhatefaizan@gmail.com>
2025-11-04 21:13:40 +0530

    Update EXP 1.cpp

--- EXP 1.cpp
+++ EXP 1.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
+#define SIZE 5
+
+class student
+{
+private:
+    char name[30];
+    int roll;
+    float marks;
+public:
+    void getdata(void)
+    {
+        cout << "Enter name: " << endl;
+        cin >> name;
+
+        cout << "Enter roll: " << endl;
+        cin >> roll;
+
+        cout << "Enter marks: " << endl;
+        cin >> marks;
+    }
+
+    void print(void)
+    {
+        cout << "Name: " << name << "\n";
+        cout << "Roll No: " << roll << "\n";
+        cout << "Marks: " << marks << "\n";
+    }
+};
 
 int main()
 {
-    cout << "HELLO WORLD! << endl;
-    return 0;
+    student s[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        s[i].getdata();
+        s[i].print();
+    }
+    
+    
 }
